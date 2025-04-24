5e66901b4d017cdd46414ace2b7fe78d09b6ccf7
Bilal-Fellah <121223453+Bilal-Fellah@users.noreply.github.com>
2025-04-24 15:08:54 +0100

    finished the main page

--- e-rep-react/src/constants/nav-items.js
+++ e-rep-react/src/constants/nav-items.js
@@ -1,5 +1,5 @@
-export const pages = [
-    { title_en: "Home", title_ar: "الرئيسية", path: "/" },
-    { title_en: "About", title_ar: "من نحن؟", path: "/about" },
-    { title_en: "Contact", title_ar: "تواصل معنا", path: "contact" },
+export const navItems = [
+    { title_en: "Features", title_ar: "الرئيسية", path: "/" },
+    { title_en: "Pricing", title_ar: "من نحن؟", path: "/about" },
+    { title_en: "Pricing", title_ar: "تواصل معنا", path: "contact" },
   ];
--- e-rep-react/src/layouts/Navigation/Header.jsx
+++ e-rep-react/src/layouts/Navigation/Header.jsx
@@ -15,15 +15,16 @@ import {
 import { useContext, useState } from "react";
 import { LanguageContext } from "../../contexts/LanguageContext";
 import { mainTheme } from "../../themes/MainTheme";
-import { pages } from "../../constants/nav-items";
+import { navItems } from "../../constants/nav-items";
 
 // icons
 import {
-  Search as SearchIcon,
   KeyboardArrowDown as ArrowDownIcon,
   Assessment as AssessmentIcon,
 } from "@mui/icons-material";
-
+import ALGIcon from "/assets/icons/algeria-icon.png";
+import USAIcon from "/assets/icons/usa-icon.png";
+import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
 export const Header = () => {
   // states & hooks
   const [anchorElNav, setAnchorElNav] = useState(null);
@@ -32,6 +33,7 @@ export const Header = () => {
   // variables from other files
   const { language, changeLanguage } = useContext(LanguageContext);
   const theme = mainTheme;
+  const headerItems = navItems;
 
   //  ui variables
   const showNav = useMediaQuery(theme.breakpoints.up(950)) ? true : false;
@@ -48,6 +50,7 @@ export const Header = () => {
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
   };
+  // console.log(headerItems);
 
   return (
     <ThemeProvider theme={theme}>
@@ -67,15 +70,88 @@ export const Header = () => {
                 RepTrack
               </Box>
             </Typography>
-            <Button color="primary" variant="text">
-              Features
-            </Button>
-            <Button color="primary" variant="text">
-              Pricing
-            </Button>
-            <Button color="primary" variant="text">
-              About
-            </Button>
+
+            {showNav ? (
+              <Box>
+                {headerItems &&
+                  headerItems.length > 0 &&
+                  headerItems.map((item, index) => {
+                    return (
+                      <Button key={index} color="primary" variant="text">
+                        {item.title_en}
+                      </Button>
+                    );
+                  })}
+              </Box>
+            ) : (
+              <Box>
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleOpenNavMenu}
+                >
+                  <MenuRoundedIcon />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorElNav}
+                  anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "left",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "left",
+                  }}
+                  open={Boolean(anchorElNav)}
+                  onClose={handleCloseNavMenu}
+                  sx={{ display: { xs: "block", md: "none" } }}
+                >
+                  {headerItems.map((item, index) => (
+                    <MenuItem
+                      key={index}
+                      onClick={() => handleNavMenuClick(item.path)}
+                    >
+                      <Typography sx={{ textAlign: "center" }}>
+                        {item["title_" + language]}
+                      </Typography>
+                    </MenuItem>
+                  ))}
+
+                  <MenuItem>
+                    <Select
+                      value={language}
+                      onChange={(e) => changeLanguage(e.target.value)}
+                    >
+                      <MenuItem value="en">
+                        <IconButton>
+                          <img
+                            className="language-img"
+                            src={USAIcon}
+                            alt="english-language-icon"
+                            style={{ width: 30, height: 30 }}
+                          />
+                        </IconButton>
+                      </MenuItem>
+                      <MenuItem value="ar">
+                        <IconButton>
+                          <img
+                            className="language-img"
+                            src={ALGIcon}
+                            alt="arabic-language-icon"
+                            style={{ width: 30, height: 30 }}
+                          />
+                        </IconButton>
+                      </MenuItem>
+                    </Select>
+                  </MenuItem>
+                </Menu>
+              </Box>
+            )}
+
             <Button color="primary" variant="contained" sx={{ ml: 2 }}>
               Sign Up
             </Button>
